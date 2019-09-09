678ab4093cfc67ad1f65ecd4233a9d048f030342
Dean Morgan <deanmorgan@mediablox.co.uk>
2019-09-09 22:29:50 +0100

    Added gulpfile.js and tasks

---
+++ gulpfile.js/tasks/setup.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp');
+
+gulp.task('copy-main-scss', function(done) {
+	gulp.src('./node_modules/mb.mbfw.scss/main.scss')
+		.pipe(gulp.dest('./scss'));
+	done();
+});
+
+gulp.task('setup', gulp.series('copy-main-scss'));
---
+++ gulpfile.js/tasks/styles.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var sass = require('gulp-sass');
+
+
+gulp.task('scss', function(done) {
+	gulp.src('./scss/main.scss')
+		.pipe(sourcemaps.init())
+		.pipe(plumber())
+		.pipe(sass({
+      includePaths: ["node_modules/inuitcss", "node_modules/mb.mbfw.scss"]
+    }).on('error', sass.logError))
+		.pipe(postcss([autoprefixer()])) // uses ".browserslistrc"
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist/css'));
+	done();
+});
+
+gulp.task('styles', gulp.series('scss'));
